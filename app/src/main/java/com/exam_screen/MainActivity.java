0698baa9bbe01398a58d5f8064a16db41fa1fb17
HO SUNGMIN <sour12@naver.com>
2023-09-13 23:18:26 +0900

    update commit. using alarm.

--- app/src/main/java/com/exam_screen/MainActivity.java
+++ app/src/main/java/com/exam_screen/MainActivity.java
@@ -2,6 +2,8 @@ package com.exam_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
@@ -9,9 +11,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -20,17 +24,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private DevicePolicyManager devicePolicyManager;
-    private ComponentName componentName;
-    private PowerManager mPowerManager;
 
-    private EditText delayEditText;
-    private Button testButton;
-    private TextView offCount;
-    private TextView onCount;
-    private int screenOnCount = 0;
-    private int screenOffCount = 0;
-    private boolean testing;
+    enum STATUS {IDLE, RUNNING, FAIL};
+    public final int ALARM_ID = 1;
+
+    public DevicePolicyManager devicePolicyManager;
+    public ComponentName componentName;
+    public PowerManager mPowerManager;
+    public EditText delayEditText;
+    public EditText checkEditText;
+    public Button testButton;
+    public TextView offCount;
+    public TextView onCount;
+    public String scriptPath;
+    public boolean testing;
+    public int screenOnCount = 0;
+    public int screenOffCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +47,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* 앱 실행시, 초기설정 */
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+                | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
+                | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
         /* 화면 Off&On 권한 가져오기 */
         devicePolicyManager = (DevicePolicyManager) getApplicationContext().getSystemService(Context.DEVICE_POLICY_SERVICE);
@@ -56,9 +68,6 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction(Intent.ACTION_SCREEN_OFF);
         registerReceiver(screenReceiver, intentFilter);
 
-        /* Testing Status */
-        testing = false;
-
         /* wakeup power manager 가져오기 */
         mPowerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
 
@@ -66,16 +75,16 @@ public class MainActivity extends AppCompatActivity {
         offCount = findViewById(R.id.off_count);
         onCount = findViewById(R.id.on_count);
         delayEditText = findViewById(R.id.delay);
+        checkEditText = findViewById(R.id.check_delay);
         testButton = findViewById(R.id.button);
-        testButton.setText("START TEST");
+        setButton(STATUS.IDLE);
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (testing) {
-                    testing = false;
+                    setButton(STATUS.IDLE);
                 } else {
-                    testButton.setText("RUNNING TEST");
-                    testing = true;
+                    setButton(STATUS.RUNNING);
                     screenOnCount = 0;
                     screenOffCount = 0;
                     screenTest();
@@ -84,11 +93,60 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private BroadcastReceiver screenReceiver = new BroadcastReceiver() {
+    public void setButton(STATUS status) {
+        if (status == STATUS.IDLE) {
+            testButton.setBackgroundColor(Color.BLUE);
+            testButton.setTextColor(Color.WHITE);
+            testButton.setText("START TEST");
+            testing = false;
+        } else if (status == STATUS.RUNNING) {
+            testButton.setBackgroundColor(Color.YELLOW);
+            testButton.setTextColor(Color.BLACK);
+            testButton.setText("RUNNING TEST");
+            testing = true;
+        } else if (status == STATUS.FAIL) {
+            testButton.setBackgroundColor(Color.RED);
+            testButton.setTextColor(Color.WHITE);
+            testButton.setText("FAIL TEST");
+            testing = false;
+        }
+    }
+
+    public void screenTest() {
+        // 화면 끄기
+        devicePolicyManager.lockNow();
+        // 화면 켜기
+        scheduleAlarm(this, Integer.parseInt(delayEditText.getText().toString()));
+    }
+
+    public void scheduleAlarm(Context context, long delayMillis) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, MyAlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, ALARM_ID, intent, 0);
+
+        long triggerAtMillis = SystemClock.elapsedRealtime() + delayMillis;
+        alarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtMillis, pendingIntent);
+    }
+
+    public BroadcastReceiver screenReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
                 screenOnCount++;
+                
+                new Handler().postDelayed(new Runnable() {  // 화면 켜지고 제테스트 판단
+                    @Override
+                    public void run() {
+                        if (!testing) {
+                            Toast.makeText(context, "테스트를 중지하였습니다.", Toast.LENGTH_SHORT).show();
+                        } else if (screenOnCount == screenOffCount) {
+                            screenTest();
+                        } else {
+                            setButton(STATUS.FAIL);
+                        }
+                    }
+                }, Integer.parseInt(checkEditText.getText().toString()));
+
             } else if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
                 screenOffCount++;
             }
@@ -96,36 +154,4 @@ public class MainActivity extends AppCompatActivity {
             offCount.setText(String.valueOf(screenOffCount));
         }
     };
-
-    private void screenTest() {
-        devicePolicyManager.lockNow();  // 화면 끄기
-        wakeUpScreen();                 // 화면 켜기
-
-        // 화면 켜지고 Broadcast 받는시점이 살작 뒤? (1500ms)에 있음...
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (!testing) {
-                    testButton.setText("START TEST");
-                    Toast.makeText(MainActivity.this, "Test를 중지하였습니다.", Toast.LENGTH_SHORT).show();
-                } else if (screenOnCount == screenOffCount) {
-                    screenTest();
-                } else {
-                    testButton.setText("FAIL TEST");
-                    Toast.makeText(MainActivity.this, "On/Off Count가 다릅니다.", Toast.LENGTH_SHORT).show();
-                }
-            }
-        }, Integer.parseInt(delayEditText.getText().toString()) + 1500);
-    }
-
-    private void wakeUpScreen() {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                PowerManager.WakeLock wakeLock = mPowerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP, "MyApp:WakeLock");
-                wakeLock.acquire();
-                wakeLock.release();
-            }
-        }, Integer.parseInt(delayEditText.getText().toString()));  // eg) EditText에서 5000ms 가져옴
-    }
 }
---
+++ app/src/main/java/com/exam_screen/MyAlarmReceiver.java
@@ -0,0 +1,19 @@
+package com.exam_screen;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.PowerManager;
+
+public class MyAlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(
+                PowerManager.FULL_WAKE_LOCK | PowerManager.ACQUIRE_CAUSES_WAKEUP,
+                "MyAlarmReceiver:Wakelock"
+        );
+        wakeLock.acquire();
+        wakeLock.release();
+    }
+}
